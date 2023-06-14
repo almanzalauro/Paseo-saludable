b779903dc01ad6235f71c211c6477bb564e2c7b5
almanzalauro <lalmanza@fi.uba.ar>
2023-06-14 11:06:26 -0300

    Update: Funciones en botones

--- js/index.js
+++ js/index.js
@@ -1,27 +1,68 @@
 const txtmin = document.getElementById("initial-minute");
 const txtsec = document.getElementById("initial-second");
 const btnStart = document.getElementById("btn-start");
+const btnPausePlay = document.getElementById("btn-pause-play");
+const btnReset = document.getElementById("btn-reset");
 
-btnStart.addEventListener('click',iniciarContador);
+let intervalId;
+let isPaused = false;
 
+btnStart.addEventListener('click', initCounter);
+btnPausePlay.addEventListener('click', togglePausePlayCounter);
+btnReset.addEventListener('click', resetCounter);
 
-function iniciarContador(){
+function initCounter() {
     let min = parseInt(txtmin.value);
     let sec = parseInt(txtsec.value);
     
-    if(min>=0 && sec>=0){
-        let separteDiv=document.getElementById("separated");
-        let minuteDiv=document.getElementById("minute");
-        let secondDiv=document.getElementById("second");
-        separteDiv.style.display="block";
-    
-    
-        minuteDiv.innerHTML=min;
-        secondDiv.innerHTML=sec;
-    }    
-    else{
-        alert("Por favor ingrese valores correctos")
+    if (min >= 0 && sec >= 0) {
+        let separateDiv = document.getElementById("separated");
+        let minuteDiv = document.getElementById("minute");
+        let secondDiv = document.getElementById("second");
+        separateDiv.style.display = "block";
+        minuteDiv.innerHTML = min;
+        secondDiv.innerHTML = sec;
+
+        intervalId = setInterval(updateCounter, 1000);
+    } else {
+        alert("Por favor ingrese valores correctos");
     }
 }
 
+function updateCounter() {
+    if (!isPaused) {
+        let minuteDiv = document.getElementById("minute");
+        let secondDiv = document.getElementById("second");
+        let minute = parseInt(minuteDiv.innerHTML);
+        let second = parseInt(secondDiv.innerHTML);
 
+        if (minute === 0 && second === 0) {
+        clearInterval(intervalId);
+        return;
+        }
+
+        if (second === 0) {
+        minute--;
+        second = 59;
+        } else {
+        second--;
+        }
+
+        minuteDiv.innerHTML = minute;
+        secondDiv.innerHTML = second;
+    }
+}
+
+function togglePausePlayCounter() {
+    isPaused = !isPaused;
+    btnPausePlay.innerHTML = isPaused ? "Reproducir" : "Pausa";
+}
+
+function resetCounter(){
+    txtmin.value='';
+    txtsec.value='';
+    let minuteDiv = document.getElementById("minute");
+    let secondDiv = document.getElementById("second");
+    minuteDiv.innerHTML = '00'
+    secondDiv.innerHTML = '00'
+}
